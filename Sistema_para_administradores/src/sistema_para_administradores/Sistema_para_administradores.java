9a4eb729014574d87f71b05dd09ad4e2a92c2320
Christian Castillo <cycascovar@gmail.com>
2014-06-05 15:52:22 -0500

    First
    Added first work in progress of a migration to delphi of an application
    of mine

---
+++ Sistema_para_administradores/src/sistema_para_administradores/Sistema_para_administradores.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema_para_administradores;
+
+/**
+ *
+ * @author Christian
+ */
+public class Sistema_para_administradores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmLogin ventanaLogin = new frmLogin();
+        ventanaLogin.setVisible(true);
+    }
+}
